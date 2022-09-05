f3c4dde1821e7150b2361ff7be02ec70d62ef161
Matt Cordial <matt.cordial@gmail.com>
2022-09-04 23:05:19 -0700

    update display
    * add instructions
    * update colors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use console::Term;
+use console::{
+    Key::{ArrowDown, ArrowLeft, ArrowRight, ArrowUp, Char},
+    Term,
+};
 use r2048::*;
 use std::{error::Error, process::exit};
 
@@ -10,18 +13,20 @@ fn main() {
 }
 
 fn game_loop(term: Term, mut game: Board) -> Result<(), Box<dyn Error>> {
-    term.clear_screen()?;
-    println!("\r{game}");
+    const FOOTER: &str = "🟢 Use arrow keys ⬅️ ⬆️ ⬇️ ➡️  to shift the board. \n\
+                          ❌ Press 'q' to quit.";
+    term.hide_cursor()?;
     loop {
+        term.clear_screen()?;
+        println!("\r{game}\n{FOOTER}\n");
         match term.read_key()? {
-            console::Key::ArrowLeft => game.shift_left()?,
-            console::Key::ArrowRight => game.shift_right()?,
-            console::Key::ArrowUp => game.shift_up()?,
-            console::Key::ArrowDown => game.shift_down()?,
+            ArrowLeft => game.shift_left()?,
+            ArrowRight => game.shift_right()?,
+            ArrowUp => game.shift_up()?,
+            ArrowDown => game.shift_down()?,
+            Char(c) if c == 'Q' || c == 'q' => return Err("You bailed".into()),
             _ => continue,
         };
         game.add_random_tile()?;
-        term.clear_screen()?;
-        println!("\r{game}");
     }
 }
